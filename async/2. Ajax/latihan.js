3af7077da11d76c2c4c8577ca79585a4e81c7299
Muhamad Yazid Imani <imaniyazid@gmail.com>
2023-01-29 11:00:39 +0700

    Ajax

---
+++ async/2. Ajax/latihan.js
@@ -0,0 +1,67 @@
+
+/*
+    AJAX
+    -> pengertianny ada di buku catetan
+    -> intinya dia mengambil / mengirim data ke url lain
+
+    cara membuat : 
+    const ajax = new XMLHttpRequest()
+    ajax.open("METHOD", "URL")
+    ajax.send()
+*/
+function getProductsUrl(keyword) {
+    return `https://www.blibli.com/backend/search/products?searchTerm=${keyword}`;
+}
+
+function getProducts(keyword) {
+    // Code AJAX Here!
+    const ajax = new XMLHttpRequest()
+    const url = getProductsUrl(keyword)
+    ajax.open("GET", url)
+    ajax.send()
+
+
+    // tidak bisa dilakukan secara sync
+    // const response = JSON.parse(ajax.responseText)
+    // console.log(response)
+}
+
+function clearProducts() {
+    const productUl = document.getElementById("products");
+    productUl.textContent = "";
+}
+
+function displayProducts(data) {
+    data.data.products.forEach(product => displayProduct(product));
+}
+
+function displayProduct(product) {
+    const productLi = document.createElement("li");
+    productLi.textContent = product.name;
+
+    const productUl = document.getElementById("products");
+    productUl.appendChild(productLi);
+}
+
+function clearTableProducts() {
+    const productUl = document.getElementById("table-products");
+    productUl.textContent = "";
+}
+
+function displayTableProducts(data) {
+    const table = document.createElement("table");
+    table.setAttribute("border", 1);
+
+    let index = 0;
+    data.data.products.forEach(product => {
+        table.insertRow(index).insertCell(0).innerText = product.name;
+        index++;
+    });
+
+    const tableProduct = document.getElementById("table-products");
+    tableProduct.appendChild(table);
+}
+
+function buttonClick() {
+    getProducts(document.getElementById("keyword").value);
+}
